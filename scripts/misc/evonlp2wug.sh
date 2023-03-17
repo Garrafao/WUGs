63381909ca8fb42fc5fcd6514a92889168d263ac
choppa98 <50850519+choppa98@users.noreply.github.com>
2023-03-17 17:21:52 +0100

    Update evonlp2wug.sh

--- scripts/misc/evonlp2wug.sh
+++ scripts/misc/evonlp2wug.sh
@@ -4,7 +4,7 @@ dir=.
 mkdir -p $dir/source
 wget https://codalab.lisn.upsaclay.fr/my/datasets/download/3e22f138-ca00-4b10-a0fd-2e914892200d -nc -P $dir/source/
 mv $dir/source/3e22f138-ca00-4b10-a0fd-2e914892200d $dir/source/starting-kit.zip
-tar xvzf $dir/source/starting-kit.zip -C $dir/source/
+unzip $dir/source/starting-kit.zip -d $dir/source/
 
 for data in 'train' 'trial' 'validation'
 do

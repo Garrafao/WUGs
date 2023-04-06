c6291970599a89c1a3329e63a08278f6b98f3292
garrafao <schlecdk@gerfalke1.ims.uni-stuttgart.de>
2023-04-06 13:18:51 +0200

    correct instances for Vorwort

--- scripts/misc/judgments2instances.sh
+++ scripts/misc/judgments2instances.sh
@@ -1,7 +1,7 @@
 
 echo $(tput bold)$BASH_SOURCE$(tput sgr0)
 
-input=durel_system/upload_formats/test_uug/judgments/Aufkommen.csv
-output=durel_system/upload_formats/test_uug/instances/Aufkommen.csv
+input=durel_system/upload_formats/test_uug/judgments/Vorwort.csv
+output=durel_system/upload_formats/test_uug/instances/Vorwort.csv
 
 python3 scripts/misc/judgments2instances.py $input $output

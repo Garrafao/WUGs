85d34111027ca47367c9cf6d27ec32cda7088773
choppa98 <50850519+choppa98@users.noreply.github.com>
2023-04-28 16:03:07 +0200

    Update judgments2instances.py

--- scripts/misc/judgments2instances.py
+++ scripts/misc/judgments2instances.py
@@ -8,10 +8,11 @@ with open(judgments, encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile, delimiter='\t',quoting=csv.QUOTE_NONE,strict=True)
     rows = [row for row in reader]
 
-lemma = rows[0]['lemma']       
 pairs = {frozenset((row['identifier1'],row['identifier2'])) for row in rows}
+pair2lemma = {frozenset((row['identifier1'],row['identifier2'])) : row['lemma'] for row in rows}
 pairs = [list(pair) for pair in pairs]
-instances = [{'lemma':lemma, 'identifier1':pair[0], 'identifier2':pair[1]} for pair in pairs]
+instances = [{'lemma': pair2lemma[frozenset(pair)], 'identifier1':pair[0], 'identifier2':pair[1]} for pair in pairs]
+
 
 #print(instances)
 

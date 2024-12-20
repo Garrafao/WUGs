21fd58c827127079a1a1e32d651f5d44b1aa86ac
AinaIanemahy <paulinesander@gmx.de>
2024-12-20 10:54:35 +0100

    Updating WUGs for DURel step 1/5
    In this step, I am adding the bash files that are needed to run the new WUGs interface in DURel. I have split the process covered by run_system2.sh into three steps.
    
    1 - create graph from data
    2 - filter existing graph
    3 - cluster filtered graph
    
    These steps include more input parameters than the previous run_system2.sh.

---
+++ scripts/DURel_tool_create_graph_from_data.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+scriptsdir=${0%/*}
+
+# parameters, will be partly overridden below
+parameterfile=$scriptsdir/parameters_system2.sh
+source $parameterfile
+echo "parameterfile: $parameterfile"
+
+# folder where the data lives.
+dir=$1
+dir=${dir%/}
+echo "dir: $dir"
+
+# data2annotators: whether to anonymize the annotators
+isanonymize=$2
+echo "isanonymize: $isanonymize"
+
+# data2graph
+summarystatistic=$3
+echo "summarystatistic: $summarystatistic"
+nonvalue=$4
+echo "nonvalue: $nonvalue"
+
+# graph2plot2: parameters for visualization
+edgestyles=$5
+echo "edgestyles: $edgestyles"
+thresholdplot=$6
+echo "threshold: $thresholdplot"
+position=$7
+echo "position: $position"
+modes=$8
+echo "modes: $modes"
+
+graph_type=original
+
+rm -rf $dir/data_joint # when running multiple times (e.g. testing)
+rm -rf $dir/graphs # when running multiple times (e.g. testing)
+rm -rf $dir/plots # when running multiple times (e.g. testing)
+rm -rf $dir/stats # when running multiple times (e.g. testing)
+rm $dir/annotators.csv # when running multiple times (e.g. testing)
+
+source $scriptsdir/map_data_structure.sh
+source $scriptsdir/data2join.sh
+source $scriptsdir/data2annotators.sh
+source $scriptsdir/data2graph.sh
+
+source $scriptsdir/graph2plot2.sh

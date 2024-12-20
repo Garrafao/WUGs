39403d85591ee63f721b976371a01fab3b8f74b4
AinaIanemahy <paulinesander@gmx.de>
2024-12-20 10:54:35 +0100

    Updating WUGs for DURel step 1/5
    In this step, I am adding the bash files that are needed to run the new WUGs interface in DURel. I have split the process covered by run_system2.sh into three steps.
    
    1 - create graph from data
    2 - filter existing graph
    3 - cluster filtered graph
    
    These steps include more input parameters than the previous run_system2.sh.

---
+++ scripts/DURel_tool_cluster_filtered_graph.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+scriptsdir=${0%/*}
+
+# parameters, will be partly overridden below
+parameterfile=$scriptsdir/parameters_system2.sh
+source $parameterfile
+echo "parameterfile: $parameterfile"
+
+# folder where the data lives.
+dir=$1
+dir=${dir%/}
+echo "dir: $dir"
+
+isclean=$2
+echo "isclean: $isclean"
+
+ambiguity=$3
+echo "ambiguity: $ambiguity"
+
+algorithm=$4
+echo "algorithm: $algorithm"
+
+degree=$5
+echo "degree: $degree"
+
+ismultiple=$6
+echo "ismultiple: $ismultiple"
+
+distribution=$7
+echo "distribution: $distribution"
+
+degcorr=$8
+echo "degcorr: $degcorr"
+
+adjacency=$9
+echo "adjacency: $adjacency"
+
+degreedl=${10}
+echo "degreedl: $degreedl"
+
+iters=${11}
+echo "iters: $iters"
+
+modus=${12}
+echo "modus: $modus"
+
+# graph2plot2: parameters for visualization
+edgestyles=${13}
+echo "edgestyles: $edgestyles"
+thresholdplot=${14}
+echo "thresholdplot: $thresholdplot"
+position=${15}
+echo "position: $position"
+modes=${16}
+echo "modes: $modes"
+
+summarystatistic=${17}
+nonvalue=${18}
+
+threshold=${19}
+
+graph_type=clustered
+
+source $scriptsdir/graph2cluster2.sh
+
+source $scriptsdir/graph2plot2.sh
---
+++ scripts/DURel_tool_filter_existing_graph.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+scriptsdir=${0%/*}
+
+# parameters, will be partly overridden below
+parameterfile=$scriptsdir/parameters_system2.sh
+source $parameterfile
+echo "parameterfile: $parameterfile"
+
+# folder where the data lives.
+dir=$1
+dir=${dir%/}
+echo "dir: $dir"
+
+annotator_filter=$2
+echo "annotator_filter: $annotator_filter"
+grouping=$3
+echo "grouping: $grouping"
+t1=$4
+echo "t1: $t1"
+t2=$5
+echo "t2: $t2"
+edgefilter=$6
+echo "edgefilter: $edgefilter"
+isnanedges=$7
+echo "isnanedges: $isnanedges"
+isnannodes=$8
+echo "isnannodes: $isnannodes"
+
+# graph2plot2: parameters for visualization
+edgestyles=$9
+echo "edgestyles: $edgestyles"
+thresholdplot=${10}
+echo "threshold: $thresholdplot"
+position=${11}
+echo "position: $position"
+modes=${12}
+echo "modes: $modes"
+
+graph_type=filtered
+
+source $scriptsdir/graph2filter.sh
+
+source $scriptsdir/graph2plot2.sh

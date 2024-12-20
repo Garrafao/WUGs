7bbb2163770f5695a1a8467c64e4838647c6b7a5
AinaIanemahy <paulinesander@gmx.de>
2024-12-20 11:16:53 +0100

    Updating WUGs for DURel step 3/5
    Changing the bash scripts. This includes three main changes.
    
    1. I have completed the change of the file 'judgments.csv' to 'annotations.csv'. This change had already been present in DURel but there was an inconsistency with the repository.
    
    2. I have added a graph_type variable to distinguish both graphs and plots. graph_type can be 'original', 'filtered' and 'clustered'. This means that graph and plot files won't be overwritten when moving on to the next step in the pipeline. This also makes it easier to restart clustering without having to recreate the whole graph etc.
    
    3. I have added $dir/ before all calls to the annotator.csv file making the path absolute.

--- scripts/map_data_structure.sh
+++ scripts/map_data_structure.sh
@@ -1,7 +1,7 @@
 
 echo $(tput bold)$BASH_SOURCE$(tput sgr0)
 
-datadir=$dir/data_original
+datadir=$dir/data
 uses=($datadir/*_uses.csv)
 outdir=$dir/data
 mkdir -p $outdir
@@ -16,5 +16,8 @@ do
     mkdir -p $outdir/$word
     cp $datadir/$word\_uses.csv $outdir/$word/uses.csv 
     cp $datadir/$word\_instances.csv $outdir/$word/instances.csv 
-    cp $datadir/$word\_judgments.csv $outdir/$word/judgments.csv 
+    cp $datadir/$word\_annotations.csv $outdir/$word/annotations.csv
+    rm $datadir/$word\_uses.csv
+    rm $datadir/$word\_instances.csv
+    rm $datadir/$word\_annotations.csv
 done

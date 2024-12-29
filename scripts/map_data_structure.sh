d292559315be90963e28b75b9c298ef73e3a613c
AinaIanemahy <paulinesander@gmx.de>
2024-12-29 12:01:54 +0100

    Rename annotations.csv to judgments.csv and ensure UTF-8 encoding.

--- scripts/map_data_structure.sh
+++ scripts/map_data_structure.sh
@@ -1,6 +1,7 @@
-
+#!/bin/bash
 echo $(tput bold)$BASH_SOURCE$(tput sgr0)
-
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 datadir=$dir/data
 uses=($datadir/*_uses.csv)
 outdir=$dir/data
@@ -15,8 +16,8 @@ do
     mkdir -p $outdir/$word
     cp $datadir/$word\_uses.csv $outdir/$word/uses.csv 
     cp $datadir/$word\_instances.csv $outdir/$word/instances.csv 
-    cp $datadir/$word\_annotations.csv $outdir/$word/annotations.csv
+    cp $datadir/$word\_judgments.csv $outdir/$word/judgments.csv
     rm $datadir/$word\_uses.csv
     rm $datadir/$word\_instances.csv
-    rm $datadir/$word\_annotations.csv
+    rm $datadir/$word\_judgments.csv
 done

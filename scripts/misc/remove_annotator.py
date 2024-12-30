631dee27628905def5ea78bdd2ed0eb0e8ec6a45
garrafao <schlecdk@wiesenweihe.ims.uni-stuttgart.de>
2024-12-30 11:38:28 +0100

    push missing data

--- scripts/misc/remove_annotator.py
+++ scripts/misc/remove_annotator.py
@@ -2,20 +2,26 @@ import sys
 import csv
 
 
-[_, judgments, output_file] = sys.argv
+[_, judgments, output_file, annotator] = sys.argv
 
 with open(judgments, encoding='utf-8') as csvfile: 
-    reader = csv.DictReader(csvfile, fieldnames=['judgment', 'lemma', 'annotator', 'identifier1', 'identifier2', 'comment'], delimiter='\t',quoting=csv.QUOTE_NONE,strict=True)
+    reader = csv.DictReader(csvfile, delimiter='\t',quoting=csv.QUOTE_NONE,strict=True)
     judgments = [row for row in reader]
 
+judgments_out = []    
 for row in judgments:
-    if row['comment']==None:
-        row['comment'] = ' '
+    if not row['annotator']==annotator:
+        judgments_out.append(row)
     
-#print(judgments).blah
+#print(judgments_out).blah
 
 # Export data
 with open(output_file, 'w') as f:  
-    w = csv.DictWriter(f, ['identifier1', 'identifier2', 'annotator', 'judgment', 'comment', 'lemma'], delimiter='\t', quoting = csv.QUOTE_NONE, quotechar='')
-    w.writeheader()
-    w.writerows(judgments)
+    # write header
+    header = '\t'.join(judgments_out[0].keys())
+    f.write(header)
+    f.write('\n')
+    for row in judgments_out:
+        line = '\t'.join(row.values())
+        f.write(line)
+        f.write('\n')
--- scripts/misc/remove_annotator.sh
+++ scripts/misc/remove_annotator.sh
@@ -1,11 +1,17 @@
 
 echo $(tput bold)$BASH_SOURCE$(tput sgr0)
+scriptsdir=${0%/*}
 
-datadir=$dir/data
+indir=data/dwug_es_4.0.1
+datadir=$indir/data
 words=($datadir/*)
+outdir=$indir/data_cleaned
+annotator=gecsa
 
 for word in "${words[@]}"
 do
     echo $word
-    python3 $scriptsdir/misc/judgments2header.py $word/judgments.csv $word/judgments.csv
+    mkdir -p $outdir/$(basename "$word")
+    cp $word/uses.csv $outdir/$(basename "$word")/uses.csv
+    python3 $scriptsdir/remove_annotator.py $word/judgments.csv $outdir/$(basename "$word")/judgments.csv $annotator
 done
